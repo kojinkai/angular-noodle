4be68224f0482bdc09d91f79025702bd017f0385
Lewis Nixon <lewcfn@gmail.com>
2013-10-17 22:50:22 +0100

    adding new controlers for the banner and starting to configure the scss files

---
+++ app/scripts/controllers/banner.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('angularDemoApp')
+  .controller('BannerCtrl', function ($scope) {
+    $scope.title = 'Your 5 Minute Reddit Spree';
+  });
---
+++ app/styles/app/scripts/controllers/banner.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('stylesApp')
+  .controller('BannerCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
